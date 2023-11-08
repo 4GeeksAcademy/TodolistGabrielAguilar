67b20847810233312146a9f796bd67c19faa35ad
Aguilar234g <96350616+Aguilar234g@users.noreply.github.com>
2023-11-08 20:10:23 +0000

    lista

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,51 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import "../../styles/index.css";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [list, setList] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
+  return (
+    <div className="TodoList">
+      <h1>Todolist</h1>
+      <ul>
+        {list.length ? (
+          list.map((list) => (
+            <li key={list.id}>
+              {list.title}
+              <button
+                onClick={() => setList((list) => list.filter((l) => l.id !== list.id))}
+               
+              >
+                X
+              </button>
+            </li>
+          ))
+        ) : (
+          <li>No hay tareas, añadir tareas</li>
+        )}
+      </ul>
+	 
+      <input
+        type="text"
+        placeholder="Añadir tarea"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            const tarea = inputValue;
+            setList((list) => [...list, { title: tarea }]);
+            setInputValue("");
+          }
+        }}
+      />
+	   <div className="list-count">{list.length} item left</div>
+    
+    </div>
+  );
 };
 
 export default Home;
